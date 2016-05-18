692f50fbde741c729496e56e867f74c59b172016
Christoph Bühler <christoph.buehler@hotmail.com>
2016-05-19 01:20:14 +0200

    no success handler

--- extensions/tableOutput/SuccessHandler.php
+++ extensions/tableOutput/SuccessHandler.php
@@ -1,9 +1,9 @@
 <?php
 
-class SuccessHandler extends Handler
-{
-	function __construct($msg = '')
-	{
-		parent::__construct(0, $msg);
-	}
-}
+// class SuccessHandler extends Handler
+// {
+// 	function __construct($msg = '')
+// 	{
+// 		parent::__construct(0, $msg);
+// 	}
+// }
