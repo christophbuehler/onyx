c2a0dddbe58a52c1b1187077f870733616ce34dc
Christoph Bühler <christoph.buehler@hotmail.com>
2016-05-27 18:33:07 +0200

    debuggin session

--- libs/Bootstrap.php
+++ libs/Bootstrap.php
@@ -26,6 +26,8 @@ class Bootstrap
     // session handling
     session_save_path(getcwd() . '/session');
     Session::init();
+    
+    echo Sesssion::get('loggedIn');
 
     require 'Exceptions.php';
 
