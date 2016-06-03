20551352916fae306cf62feb10428c5b17171272
Christoph Bühler <christoph.buehler@hotmail.com>
2016-06-03 17:58:29 +0200

    test global request

--- libs/Bootstrap.php
+++ libs/Bootstrap.php
@@ -33,14 +33,14 @@ class Bootstrap
       array_push($errors, $e);
     }
 
-    return;
-
     // handle this global request
     try {
       self::handle_global_request();
     } catch(Exception $e) {
       array_push($errors, $e);
     }
+
+    return;
 
     // handle this page request
     try {
